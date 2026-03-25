27932053c2a519110f30d3c7a872e3b23b2d01e3
mjy90377 <mjy90377@gmail.com>
2026-03-25 11:58:55 +0900

    first commit

---
+++ react1/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+
+import reactLogo from '../assets/react.svg'
+
+
+export default function Profile () {
+  return (
+    <>
+        <img className="button-icon" src={reactLogo} alt="" />
+    </>
+  )
+}
