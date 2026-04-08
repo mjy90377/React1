38f04661b18c2ad0c2552ab770c48ba948335bfb
mjy90377 <mjy90377@gmail.com>
2026-04-08 12:34:58 +0900

    변수 사용

--- react1/src/components/Items.jsx
+++ react1/src/components/Items.jsx
@@ -1,12 +1,11 @@
 export default function Items({name, isPacked}){
+    let itemContent = name;
+    if(isPacked) {
+        itemContent = <del>{name + "☑️"}</del>
+    }
     return(
         <li>
-            {isPacked ? (
-                <del>
-                    {name + "☑️"} 
-                </del>
-            ) : ( name
-            )}
+            {itemContent}
         </li>
     )
 }
