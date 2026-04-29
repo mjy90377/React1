ab38dec51542823c6d461f3a554a8ce07dee5155
mjy90377 <mjy90377@gmail.com>
2026-04-29 12:35:17 +0900

    이벤트 핸들러

--- react1/src/components/ButtonCom/ButtonCom.jsx
+++ react1/src/components/ButtonCom/ButtonCom.jsx
@@ -1,11 +1,15 @@
 import style from "./ButtonCom.module.css"
 
 export default function ButtonCom(){
+    function handleClick(){
+        alert('버튼 클릭');
+    }
+
     return (
         <>
             <h1 className={style.title}>ButtonCom</h1>
             <nav className={style.navBar}>
-                <button>버튼1</button>
+                <button onClick={handleClick}>버튼1</button>
                 <button>버튼2</button>
             </nav>
         </>
