625c8fcd472a302c91b26f7e42fea356e4be2477
mjy90377 <mjy90377@gmail.com>
2026-05-13 11:35:18 +0900

    이벤트 전파 막기

--- react1/src/components/Bubble/Bubble.jsx
+++ react1/src/components/Bubble/Bubble.jsx
@@ -1,12 +1,29 @@
 import style from "./Bubble.module.css"
 
+function Button({onClick, children}){
+    return(
+        <>  
+            <button className={style.button} onClick={e => {
+                e.stopPropagation();
+                onClick();
+            }}>
+                {children}
+            </button>
+        </>
+    )
+}
+
 export default function Bubble(){
     return(
         <>
             <h1 className={style.title}>Bubble</h1>
             <nav className={style.navBar} onClick={() => alert("내비게이션바 클릭")}>
-                <button onClick={() => alert("버튼1 클릭")} className={style.button}>버튼1</button>&nbsp;
-                <button onClick={() => alert("버튼2 클릭")} className={style.button}>버튼2</button>
+                <Button onClick={() => alert("버튼1 클릭")}>
+                    버튼1
+                </Button>&nbsp;
+                <Button onClick={() => alert("버튼2 클릭")}>
+                    버튼2
+                </Button>
             </nav>
         </>
     )
